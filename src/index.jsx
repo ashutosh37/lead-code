0599cf14daaeb4186f87e1f1e0c1f748da71fdeb
Ashutosh Nigam <ashutosh.nigam@empired.com>
2018-05-07 09:50:51 +1000

    new changes

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Container,Media , Row, Col,Modal, ModalHeader, ModalBody, ModalFooter,Jumbotron , Button } from 'reactstrap';
 import {Header} from './components/Header/Header';
 import Logo from './images/Krishail_Logo.jpg';
-import ProductImage from './images/img_venetta_full_opt.png';
+import ProductImage from './images/product.jpg';
 import AmazonLogo from './images/amazonlogo.png';
 import MailChimpContainer from './components/MailChimp/MailChimpContainer';
 import {CheckIcon} from './components/Constants'
@@ -24,16 +24,16 @@ class MainContainer extends React.Component {
                <img src={Logo} alt="KRISHAIL Logo" style={{width:'150px'}}/>
             </Col>
             <Col md="8" className="Header">
-            <p>Krishail Anti Aging Cream SAVE <span style={{color:'red'}}>20%</span> With Your Coupon Code</p>
+            <p>Hornbull Mens Leather RFID Blocking Wallet Save <span style={{color:'red'}}>20%</span> With Your Coupon Code</p>
             </Col>
               <Col md="12" className="Description">
               <Jumbotron>
-              <h2>Angi aging scream for radiant skin - now just a click away</h2>
+              <h2>Hornbull Mens Leather RFID Blocking Wallet (Brown) - now just a click away</h2>
                <ul style={{listStyle:'none'}} className="bulletpoints">
-                   <li><CheckIcon/><span>ANTI FOG: Swim Goggles inner surface 
-                     of lens are coated with anti-fog technology which enhances the anti-fog ability of the swimming goggles and does no any harm to your eyes and provide crystal clear vision</span></li>
-                   <li><CheckIcon/><span>ANTI FOG: Swim Goggles inner surface of lens are coated with anti-fog technology which enhances the anti-fog ability of the swimming goggles and does no any harm to your eyes and provide crystal clear vision</span></li>
-                   <li><CheckIcon/><span>ANTI FOG: Swim Goggles inner surface of lens are coated with anti-fog technology which enhances the anti-fog ability of the swimming goggles and does no any harm to your eyes and provide crystal clear vision</span></li>
+                   <li><CheckIcon/><span>TOP GRAIN LEATHER: This Wallet Made in Exclusive Leather, Well Designed By Skilled Handcraft Man, Durable Cloth Lining Used, Designed To Accommodate maximum needs. Each Wallet Has Unique Natural Grain Of Leather.</span></li>
+                   <li><CheckIcon/><span>RFID BLOCKING SECURITY: Our wallets are equipped with advanced RFID SECURE Technology, a unique metal composite, engineered specifically to block 13.5 MHz or higher RFID signals and protect the valuable information stored on RFID chips from unauthorized scans.</span></li>
+                   <li><CheckIcon/><span>MECHANISM: There are 6 in build Credit card slots, one Transparent Identity window for Driving License or Office I.D card, two secret compartment, 1 Coin Pocket and 2 Currency compartment carrying all your needs with extra comfort while carrying.</span></li>
+                   <li><CheckIcon/><span>GIFTING SOLUTION: This wallet of Hornbull is ideal to gift your loved one and it comes with gift box.</span></li>
                </ul>
               </Jumbotron>
             </Col>
